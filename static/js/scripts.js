f2086fb31096c3dc157639aa534f1366a50e58f3
Guilherme Mattar Bastos <guilhermembastos@gmail.com>
2015-09-09 10:43:22 -0300

    Mobile improvements.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,2 +1,11 @@
 $(document).ready(function($) {
+    $('.e-menu').click(function() {
+        $('.b-menu').addClass('m-active');
+
+        return false;
+    });
+
+    $('body:not(.b-menu)').click(function() {
+        $('.b-menu').removeClass('m-active');
+    });
 });
